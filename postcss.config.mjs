62986b9661ce34df4b42bc699632a1dce02f4d86
Isru10 <149589109+Isru10@users.noreply.github.com>
2026-06-05 23:26:53 +0300

    Remove PostCSS configuration file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,1 @@
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
-
-
-const config = { plugins: { "@tailwindcss/postcss": {}, }, };  export default config;
 
